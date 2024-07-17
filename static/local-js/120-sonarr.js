88b9b6bd192aee796d991a14f035516358e53238
bullmoose20 <12549033+bullmoose20@users.noreply.github.com.>
2024-07-17 17:33:15 -0400

    sonarr dynamic dropdowns

--- static/local-js/120-sonarr.js
+++ static/local-js/120-sonarr.js
@@ -1,4 +1,4 @@
-/* global $, validateButton, showSpinner, hideSpinner */
+/* global $, initialSonarrRootFolderPath, initialSonarrQualityProfile, initialSonarrLanguageProfile, showSpinner, hideSpinner */
 
 $(document).ready(function () {
   const isValidated = document.getElementById('sonarr_validated').value.toLowerCase()
@@ -7,6 +7,8 @@ $(document).ready(function () {
 
   if (isValidated === 'true') {
     document.getElementById('validateButton').disabled = true
+    // Populate the dropdowns with the stored data if they are available
+    fetchDropdownData()
   } else {
     document.getElementById('validateButton').disabled = false
   }
@@ -19,12 +21,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
 document.getElementById('sonarr_token').addEventListener('input', function () {
   document.getElementById('sonarr_validated').value = 'false'
-  validateButton.disabled = false
+  document.getElementById('validateButton').disabled = false
 })
 
 document.getElementById('sonarr_url').addEventListener('input', function () {
   document.getElementById('sonarr_validated').value = 'false'
-  validateButton.disabled = false
+  document.getElementById('validateButton').disabled = false
 })
 
 /* eslint-disable camelcase */
@@ -52,9 +54,9 @@ function validateSonarrApi () {
         statusMessage.style.display = 'block'
         document.getElementById('validateButton').disabled = true
 
-        populateDropdown('sonarr_root_folder_path', data.root_folders, 'path', 'path')
-        populateDropdown('sonarr_quality_profile', data.quality_profiles, 'name', 'name')
-        populateDropdown('sonarr_language_profile', data.language_profiles, 'name', 'name')
+        populateDropdown('sonarr_root_folder_path', data.root_folders, 'path', 'path', initialSonarrRootFolderPath)
+        populateDropdown('sonarr_quality_profile', data.quality_profiles, 'name', 'name', initialSonarrQualityProfile)
+        populateDropdown('sonarr_language_profile', data.language_profiles, 'name', 'name', initialSonarrLanguageProfile)
       } else {
         hideSpinner('validate')
         document.getElementById('sonarr_validated').value = 'false'
@@ -73,9 +75,30 @@ function validateSonarrApi () {
       document.getElementById('sonarr_validated').value = 'false'
     })
 }
-/* eslint-enable camelcase */
+function fetchDropdownData () {
+  // Fetch the stored dropdown data and populate the dropdowns
+  const sonarr_url = document.getElementById('sonarr_url').value
+  const sonarr_token = document.getElementById('sonarr_token').value
 
-function populateDropdown (elementId, data, valueField, textField) {
+  fetch('/validate_sonarr', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ sonarr_url, sonarr_token })
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      populateDropdown('sonarr_root_folder_path', data.root_folders, 'path', 'path', initialSonarrRootFolderPath)
+      populateDropdown('sonarr_quality_profile', data.quality_profiles, 'name', 'name', initialSonarrQualityProfile)
+      populateDropdown('sonarr_language_profile', data.language_profiles, 'name', 'name', initialSonarrLanguageProfile)
+    })
+    .catch(error => {
+      console.error('Error fetching Sonarr dropdown data:', error)
+    })
+}
+/* eslint-enable camelcase */
+function populateDropdown (elementId, data, valueField, textField, selectedValue = '') {
   const dropdown = document.getElementById(elementId)
   dropdown.innerHTML = '<option value="">Select an option</option>'
 
@@ -85,6 +108,10 @@ function populateDropdown (elementId, data, valueField, textField) {
     option.textContent = item[textField]
     dropdown.appendChild(option)
   })
+
+  if (selectedValue) {
+    dropdown.value = selectedValue
+  }
 }
 
 function toggleApiKeyVisibility () {
