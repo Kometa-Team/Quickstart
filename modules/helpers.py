a6cb6b0348873b5729eaa8f6447acb6669ee7509
GitHub Action <action@github.com>
2025-03-18 19:31:43 +0000

    Code Format

--- modules/helpers.py
+++ modules/helpers.py
@@ -18,7 +18,7 @@ GITHUB_BASE_URL = "https://raw.githubusercontent.com/Kometa-Team/Kometa"
 
 BASE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
 WORKING_DIR = os.path.dirname(sys.executable) if getattr(sys, "frozen", False) else BASE_DIR
-MEIPASS_DIR = sys._MEIPASS if getattr(sys, "frozen", False) else BASE_DIR # noqa
+MEIPASS_DIR = sys._MEIPASS if getattr(sys, "frozen", False) else BASE_DIR  # noqa
 JSON_SCHEMA_DIR = os.path.join(WORKING_DIR, "json-schema")
 os.makedirs(JSON_SCHEMA_DIR, exist_ok=True)
 CONFIG_DIR = os.path.join(WORKING_DIR, "config")
