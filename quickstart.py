b5b5dd7f98d52633f64df361efe1644bad4c7db1
GitHub Action <action@github.com>
2025-03-20 21:03:45 +0000

    Code Format

--- quickstart.py
+++ quickstart.py
@@ -863,9 +863,9 @@ def validate_notifiarr():
         return jsonify(result.get_json()), 400
 
 
-@app.route('/shutdown')
+@app.route("/shutdown")
 def shutdown():
-    func = request.environ.get('werkzeug.server.shutdown')
+    func = request.environ.get("werkzeug.server.shutdown")
     if func:
         func()
     return "Shutting down...", 200
@@ -1011,8 +1011,7 @@ if __name__ == "__main__":
                     print("[INFO] Restarting Quickstart via Python script...")
                     subprocess.Popen([sys.executable] + sys.argv, cwd=os.getcwd())
 
-                os._exit(0) # noqa
-
+                os._exit(0)  # noqa
 
             def exit_action(self, icon):  # noqa
                 global server_thread, update_thread
