ea2d74663ee61560b705082e1d235c0a8bcdaeb6
bullmoose20 <12549033+bullmoose20@users.noreply.github.com.>
2026-01-10 22:11:27 -0500

    added sysinfo

--- modules/output.py
+++ modules/output.py
@@ -39,7 +39,7 @@ def section_heading(title, font="standard"):
 
 def clean_section_data(section_data, config_attribute):
     """
-    Cleans out temporary or irrelevant data before integrating it into the final config
+    Cleans out temporary or irrelevant data before integrating it into the final config.yml
     """
     clean_data = {}
 
