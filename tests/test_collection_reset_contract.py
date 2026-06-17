de884ec3140069a7c8c41460e1fbe842e8cd393b
GitHub Action <action@github.com>
2026-06-17 12:12:45 +0000

    Code Format

--- tests/test_collection_reset_contract.py
+++ tests/test_collection_reset_contract.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-
 MACROS_PATH = Path(__file__).resolve().parents[1] / "templates" / "partials" / "_macros.html"
 
 
@@ -15,6 +14,6 @@ def test_collection_template_variables_include_reset_button():
 def test_collection_reset_contract_exposes_defaults_for_supported_field_types():
     text = MACROS_PATH.read_text(encoding="utf-8")
 
-    assert 'data-default="{{ item.default | default(\'false\', true) }}"' in text
-    assert 'data-default="{{ item.default | default(\'\', true) }}"' in text
+    assert "data-default=\"{{ item.default | default('false', true) }}\"" in text
+    assert "data-default=\"{{ item.default | default('', true) }}\"" in text
     assert "data-default='{{ list_default_json }}'" in text
