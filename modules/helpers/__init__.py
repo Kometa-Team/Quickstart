561308dec5e6d2de725f5ddb907a827acaccaf95
Chaz Larson <chaz@chazlarson.com>
2026-06-30 17:05:40 -0500

    [116] Extract _fonts.py + _overlays.py from _legacy.py (#1407)

--- modules/helpers/__init__.py
+++ modules/helpers/__init__.py
@@ -27,4 +27,6 @@ from ._legacy import (  # noqa: F401
 # will override legacy definitions (in case of a future rename).
 from ._cli import *  # noqa: F403
 from ._templates import *  # noqa: F403
+from ._fonts import *  # noqa: F403
+from ._overlays import *  # noqa: F403
 from ._paths import *  # noqa: F403
