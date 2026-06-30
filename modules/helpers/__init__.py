905ba731b6b2a22b9eb2cf5dc066786bdf60fa9c
Chaz Larson <chaz@chazlarson.com>
2026-06-30 18:44:18 -0500

    Extract _git.py from _legacy.py (#1414)

--- modules/helpers/__init__.py
+++ modules/helpers/__init__.py
@@ -39,4 +39,5 @@ from ._restart import *  # noqa: F403
 from ._redact import *  # noqa: F403
 from ._file_utils import *  # noqa: F403
 from ._settings import *  # noqa: F403
+from ._git import *  # noqa: F403
 from ._paths import *  # noqa: F403
