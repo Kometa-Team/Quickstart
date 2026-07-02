5a5385e87180193a52bc642ccab74152ceb8ad9b
Chaz Larson <chaz@chazlarson.com>
2026-07-02 00:07:02 -0500

    [146] Extract _kometa_paths.py from _legacy.py (#1440)

--- modules/helpers/__init__.py
+++ modules/helpers/__init__.py
@@ -50,6 +50,7 @@ from ._install_mode import *  # noqa: F403
 from ._kometa_version import *  # noqa: F403
 from ._pid import *  # noqa: F403
 from ._paths import *  # noqa: F403
+from ._kometa_paths import *  # noqa: F403
 from ._zip_update import *  # noqa: F403
 from ._logging import *  # noqa: F403
 from ._plex import *  # noqa: F403
